50e1451c3bba65aee3a97b6278eeb620d281cb7e
jhordycamacas <jhordycamacas9@gmail.com>
2024-12-05 11:04:26 -0500

    cambios1

--- ManejoCicloFor/src/paquete01/Ejemplo011.java
+++ ManejoCicloFor/src/paquete01/Ejemplo011.java
@@ -17,9 +17,9 @@ public class Ejemplo011 {
     public static void main(String[] args) {
         
         for (int i = 10; i >= 0; i--) {
-            System.out.printf("%d\n", i);
+            System.out.printf("%d\n", i);         
         }
-        
+                                     
         
     }
     
