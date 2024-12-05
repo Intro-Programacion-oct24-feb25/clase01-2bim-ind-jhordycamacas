422a02d28bb78ddc8e14de1c39109d742a05d92d
jhordycamacas <jhordycamacas9@gmail.com>
2024-12-05 12:45:44 -0500

    cambios7

--- ManejoCicloFor/src/paquete04/Ejercicio02.java
+++ ManejoCicloFor/src/paquete04/Ejercicio02.java
@@ -11,21 +11,27 @@ package paquete04;
 public class Ejercicio02 {
 
     public static void main(String[] args) {
-        double celcius = 0;
+        double celcius;
         double farenheit;
         int operacion;
         String cadenafinal = "";
+        String cadenafinal2 = "";
 
         for (operacion = 0; operacion <= 20; operacion++) {
-            for (farenheit = 20; farenheit < 100; farenheit += 4) {
-
-                celcius = 5 / 9 * (farenheit - 32.00);
-                System.out.printf("Operacion (%d) Farenheit: %.2f ===> Celcius: %.2f\n", operacion,
+            for (farenheit = 20; farenheit <= 100; farenheit += 4) {
+                celcius  = 5 / 9 * (farenheit - 32);
+                cadenafinal = String.format("%sOperacion (%d) Farenheit: %.2f ===> Celcius: %.2f\n",
+                        cadenafinal,
+                        operacion,
                         farenheit,
                         celcius);
+
             }
 
+            cadenafinal2 = String.format("%s%s", cadenafinal2, cadenafinal);
+            cadenafinal = "";
         }
 
+        System.out.printf("%s", cadenafinal2);
     }
 }
