63c10c404a574fca3b340e9f3b58af5ab1819648
jhordycamacas <jhordycamacas9@gmail.com>
2024-12-05 11:40:41 -0500

    cambios5

--- ManejoCicloFor/src/paquete04/Ejercicio01.java
+++ ManejoCicloFor/src/paquete04/Ejercicio01.java
@@ -9,5 +9,21 @@ package paquete04;
  * @author utpl
  */
 public class Ejercicio01 {
-    
+      public static void main(String[] args) {
+       int num;
+       int operacion;
+       String cadenafinal = "";
+       String cadenafinal2 = "";
+       for (num = 30; num >= 10;num--){
+           
+        for (int contador = 1; contador <= 4; contador ++){
+           operacion = num * contador;
+           cadenafinal = String.format("%s%d-",cadenafinal,operacion);
+           
+        }
+        cadenafinal2 = String.format("%s%s\n",cadenafinal2, cadenafinal);
+        cadenafinal = "";
+       }
+       System.out.printf("%s\n",cadenafinal2);
+    }
 }
